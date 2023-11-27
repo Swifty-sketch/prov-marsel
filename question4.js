7827b4d6756bac189183d1eae73da0d2ba1cbe3d
Swifty-sketch <marsel.davletov@chasacademy.se>
2023-11-27 10:51:54 +0100

    first commit2

--- question4.js
+++ question4.js
@@ -1,25 +1,36 @@
-// 3. Shoppingcart (3p)
-// Skriv klart funktionen calculateTotalPrice som tar en array med produkter
-// som input och returnerar det totala priset
+// 4. Shoppingcart med separata priser (3p)
+// Skriv klart funktionen calculateTotalPrice2 som tar en array med produkter
+// och ett objekt med priser som input och returnerar det totala priset
 
-const products = [
-    { name: "Camera", brand: "Canon", model: "EOS_70D", price: 100 },
-    { name: "Camera", brand: "Nikon", model: "D3400", price: 120 },
-  ];
-  
-  const products2 = [
-    { name: "Camera", brand: "GoPro", model: "Hero_4", price: 80 },
-    { name: "Drone", brand: "DJI", model: "Phantom", price: 50 },
-    { name: "Drone", brand: "GoPro", model: "Karma", price: 200 },
-  ];
-  
-  function calculateTotalPrice(shoppingCart) {
-    var total=0;
-    for (let i = 0; i < shoppingCart.length; i++) {
-        total += shoppingCart[i]["price"] ;
-    }
-    return total;
-  }
-  
-  console.log(calculateTotalPrice(products)); // Ska logga 220
-  console.log(calculateTotalPrice(products2)); // Ska logga 330
+// 4. Shoppingcart med separata priser (3p)
+// Skriv klart funktionen calculateTotalPrice2 som tar en array med produkter
+// och ett objekt med priser som input och returnerar det totala priset
+
+const modelPrices = {
+  EOS_70D: 100,
+  D3400: 120,
+  Hero_4: 80,
+  Phantom: 50,
+  Karma: 200,
+};
+
+const shoppingCart = [
+  { name: "Camera", brand: "Canon", model: "EOS_70D" },
+  { name: "Camera", brand: "Nikon", model: "D3400" },
+  { name: "Camera", brand: "GoPro", model: "Hero_4" },
+  { name: "Drone", brand: "DJI", model: "Phantom" },
+  { name: "Drone", brand: "GoPro", model: "Karma" },
+];
+
+function calculateTotalPrice2(shoppingCart, prices) {
+  var total = 0;
+
+  shoppingCart.forEach((product) => {
+    const price = prices[product.model];
+    total += price;
+  });
+
+  return total;
+}
+
+console.log(calculateTotalPrice2(shoppingCart, modelPrices)); // Ska logga 550
